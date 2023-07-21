ce8ca6feeba35dcff33ee114239dde8dfb8dda1f
fmora122 <pfmoras@gmail.com>
2023-07-21 17:55:45 -0600

    finished assigment after deadline

--- Ejercicio3/src/ejercicio3/Ejercicio3.java
+++ Ejercicio3/src/ejercicio3/Ejercicio3.java
@@ -16,14 +16,15 @@ public class Ejercicio3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //Feligreses feligreses[] = new Feligreses[1];
+        int size = 0;
+        Feligreses feligreses[] = new Feligreses[size];
         int option = 1;
         while (option != 0) {
             System.out.println("Main Menu - Select an option");
             System.out.println("1) To add a Pastor.");
             System.out.println("2) To add an Iglesia.");
             System.out.println("3) To add Feligreses");
-            System.out.println("4) To exit.");
+            System.out.println("4) Diezmo Analytics.");
             System.out.println("0) To exit.");
 
             System.out.println("");
@@ -38,9 +39,14 @@ public class Ejercicio3 {
                 iglesia.setNameIglesia(iglesia.collectIglesia());
             }
             if (option == 3) {
+                //System.out.println(size);
+                //System.out.println(feligreses.length);
                 Count count = new Count();
-                int size = count.countfeligreses();
-                Feligreses feligreses[] = new Feligreses[size];
+                size = count.countfeligreses();
+                //System.out.println(size);
+                //System.out.println(feligreses.length);
+                //Feligreses feligreses[];
+                feligreses = new Feligreses[size];
                 for(int x=0; x<feligreses.length; x++){
                     String nameFeligreses = JOptionPane.showInputDialog("Enter the Feligreses' name");
                     int cedula = Integer.parseInt(JOptionPane.showInputDialog("Enter the Feligreses' cedula"));
@@ -50,17 +56,71 @@ public class Ejercicio3 {
 
             }
             if (option == 4) {
-                System.out.println(feligreses[0]);
-            }
+                //System.out.println(feligreses[0].getNameFeligreses());
+                //System.out.println(feligreses[1].getNameFeligreses());
+                
+                int option4 = 1;
+                while (option4 != 0) {
+                    System.out.println("1) Total de ganancias de la iglesia");
+                    System.out.println("2) Municipalidad infraestructura");
+                    System.out.println("3) Comedor municipal");
+                    System.out.println("4) Ganancia para el pastor");
+                    System.out.println("5) Nombres y cédulas de las personas que el diezmo con valor 0");
+                    System.out.println("6) Nombres y cédulas de las personas con diezmo mayor a 100000");
+                    System.out.println("0) Exit");
+                    System.out.println("");
+                    option = Integer.parseInt(JOptionPane.showInputDialog("Type your selection"));
+                    
+                    int total = 0;
+                    for (Feligreses feligrese : feligreses) {
+                        total += feligrese.getDiezmo();
+                    }
+
+                    if(option == 1) {
+                        System.out.println("El Total de ganancias de la iglesia es: "+total);
+                        System.out.println("\n\n");
+                        
+                    }
+                    if(option == 2) {
+                        //double newtotal = total*0.09;
+                        System.out.println("El Total para Municipalidad infraestructura es: "+total*0.09);
+                        System.out.println("\n\n");
+                    }
+                    if(option == 3) {
+                        System.out.println("El Total para Comedor municipal es: "+total*0.21);
+                        System.out.println("\n\n");
+                    }
+                    if(option == 4) {
+                        System.out.println("El Total de Ganancia para el pastor es: "+total*0.7);
+                        System.out.println("\n\n");
+                    }
+                    if(option == 5) {
+                        System.out.println("El Nombres y cédulas de las personas que el diezmo con valor 0");
+                        for (Feligreses feligrese : feligreses) {
+                            if (feligrese.getDiezmo() == 0) {
+                                System.out.println("Nombre: " + feligrese.getNameFeligreses());
+                                System.out.println("Cedula: " + feligrese.getCedula());
+                                System.out.println("");
+                            }
+                        }
+                        
+                    }
+                    if(option == 6) {
+                        System.out.println("El Nombres y cédulas de las personas con diezmo mayor a 100000");
+                        for (Feligreses feligrese : feligreses) {
+                            int amount = feligrese.getDiezmo();
+                            if (amount > 10000) {
+                                System.out.println("Nombre: " + feligrese.getNameFeligreses());
+                                System.out.println("Cedula: " + feligrese.getCedula());
+                            }
+                        }
+                
+                    }
             
+		}
+				
+            }
+        
         }
-        
-        
-        
-        
     }
-
-
-
-    
 }
